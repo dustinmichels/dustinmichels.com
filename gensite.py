fc41fcb4dad777e6dcc98f31b82b8a76cbf7201a
Dustin Michels <dustin7538@gmail.com>
2018-06-25 13:50:54 -0600

    with bs4

--- gensite.py
+++ gensite.py
@@ -1,5 +1,41 @@
-# Make that website, baby
+"""
+Jinja2 script for making website!
+"""
 
-from jinja2 import Template
-template = Template('Hello {{ name }}!')
-template.render(name='John Doe')
+import sys
+import os
+import shutil
+from jinja2 import Environment, FileSystemLoader, Markup, select_autoescape
+from bs4 import BeautifulSoup
+
+IN_DIR = './templates'
+OUT_DIR = '_build'
+
+env = Environment(
+    loader=FileSystemLoader(IN_DIR),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+if __name__ == '__main__':
+
+    # make build dir
+    if os.path.exists(OUT_DIR):
+        shutil.rmtree(OUT_DIR)
+    os.makedirs(OUT_DIR)
+
+    # copy assets, scripts, styles into build dir
+    shutil.copytree('assets', '_build/assets')
+    shutil.copytree('scripts', '_build/scripts')
+    shutil.copytree('styles', '_build/styles')
+
+    # render each page in IN_DIR to OUT_DIR
+    pages = [p for p in os.listdir(IN_DIR) if p != 'base.html']
+    for page in pages:
+        
+        template = env.get_template(page)
+        html_doc = template.render()
+        soup = BeautifulSoup(html_doc, 'html.parser')
+        new_html = soup.prettify()
+
+        with open(f'{OUT_DIR}/{page}', 'w') as f:
+            f.write(new_html)
